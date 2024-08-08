4199b7fc7f9f4f7f01c736623a838572f4cf5ccc
lochjulio <juliocesarlochstupp@gmail.com>
2024-08-08 12:53:16 -0300

    primeiros exercicios

---
+++ src/n2ex7.java
@@ -0,0 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class n2ex7 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double x, y;
+
+        x = sc.nextDouble();
+        y = sc.nextDouble();
+
+        if (x == 0 && y == 0) {
+            System.out.println("Origem");
+        }
+        else if (x == 0) {
+            System.out.println("Eixo X");
+        }
+        else if (y == 0) {
+            System.out.println("Eixo y");
+        }
+        else if (x > 0 && y > 0) {
+            System.out.println("Q1");
+        }
+        else if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        }
+        else if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        }
+        else {
+            System.out.println("Q4");
+        }
+
+        sc.close();
+    }
+}
