7a448f553552f3d57b14a88a2e0c273d45b50c3e
lochjulio <juliocesarlochstupp@gmail.com>
2024-08-08 11:58:07 -0300

    primeiros exercicios

--- src/n2ex6.java
+++ src/n2ex6.java
@@ -1,12 +1,29 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class n2ex6 {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double valor;
+        double valor = sc.nextDouble();
 
-        if (valor>0 || valor<25)
+        if (valor < 0.0 || valor > 100) {
+            System.out.println("Fora de intervalo");
+        }
+        else if (valor <= 25) {
+            System.out.println("Intervalo [0,25)");
+        }
+        else if (valor <= 50) {
+            System.out.println("Intervalo (25,50)");
+        }
+        else if (valor <= 75) {
+            System.out.println("Intervalo (50,75)");
+        }
+        else {
+            System.out.println("Intervalo (75,100]");
+        }
 
         sc.close();
     }
