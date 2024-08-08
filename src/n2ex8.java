0129e2a1b48a29b93bbe4b7269e3ad13a03ab007
lochjulio <juliocesarlochstupp@gmail.com>
2024-08-08 13:11:02 -0300

    primeiros exercicios

---
+++ src/n2ex8.java
@@ -0,0 +1,36 @@
+import javax.sound.sampled.FloatControl;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class n2ex8 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double valor = sc.nextDouble();
+        double imposto;
+
+        if (valor <= 2000.00) {
+            imposto = 0.0;
+        }
+        else if (valor <= 3000.00) {
+            imposto = (valor - 2000.00) * 0.08;
+        }
+        else if (valor <= 4500.00) {
+            imposto = (valor - 3000.00) * 0.18 + 1000.00 * 0.08;
+        }
+        else {
+            imposto = (valor - 4500.00) * 0.28 + 1500.00 * 0.18 + 1000.00 * 0.08;
+        }
+
+        if (imposto == 0.0) {
+            System.out.println("Isento");
+        }
+        else {
+            System.out.printf("R$ %.2f%n", imposto);
+        }
+
+        sc.close();
+    }
+}
