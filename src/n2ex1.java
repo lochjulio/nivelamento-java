19e2e58da5d7c59c40e014406df5b2bcac87a65a
lochjulio <juliocesarlochstupp@gmail.com>
2024-08-08 00:06:33 -0300

    primeiros exercicios

---
+++ src/n2ex1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class n2ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        if (N<0) {
+            System.out.println("NEGATIVO");
+        }
+        else {
+            System.out.println("POSITIVO");
+        }
+
+        sc.close();
+    }
+}
