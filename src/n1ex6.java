afa91671b3878a9228d882720dd8f532636b5039
lochjulio <juliocesarlochstupp@gmail.com>
2024-08-08 00:01:22 -0300

    primeiros exercicios

---
+++ src/n1ex6.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class n1ex6 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double A, B, C, tri, circ, trap, quad, ret;
+
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+        C = sc.nextDouble();
+
+        tri = A * C / 2.0;
+        circ = 3.14159 * C * C;
+        trap = (A + B) / 2.0 * C;
+        quad = B * B;
+        ret = A * B;
+
+        System.out.printf("TRIANGULO: %.3f%n", tri);
+        System.out.printf("CIRCULO: %.3f%n", circ);
+        System.out.printf("TRAPEZIO: %.3f%n", trap);
+        System.out.printf("QUADRADO: %.3f%n", quad);
+        System.out.printf("RETANGULO: %.3f%n", ret);
+
+        sc.close();
+    }
+}
